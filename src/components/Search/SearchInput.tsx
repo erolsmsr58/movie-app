90d2915d34dd0133d8b5b2a0390c2f68fdb89764
Erol Simsir <info@graphity.nl>
2025-04-09 00:33:50 +0200

    Add some accessiblity to the search form and improve search test

--- src/components/Search/SearchInput.tsx
+++ src/components/Search/SearchInput.tsx
@@ -7,9 +7,13 @@ const SearchInput = ({ query, setQuery }: SearchInputProps): ReactElement => {
     return (
         <Stack direction={{ base: "column", sm: "row" }} gap={3}>
             <Input
+                id="search"
+                type="search"
                 value={query}
                 onChange={(event) => setQuery(event.target.value)}
                 placeholder="Search movie title..."
+                aria-label="Movie title"
+                role="searchbox"
             />
         </Stack>
     );
