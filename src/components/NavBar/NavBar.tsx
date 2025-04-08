795505c1c8015ecdd4e6145723be8ae3a5d90e6e
Erol Simsir <info@graphity.nl>
2025-04-08 09:50:27 +0200

    Minor clean up

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,7 +1,9 @@
+import { ReactElement } from "react";
 import { HStack } from "@chakra-ui/react";
+
 import NavLinkButton from "./NavLinkButton";
 
-const NavBar = () => (
+const NavBar = (): ReactElement => (
   <HStack gap={4} mb={6}>
     <NavLinkButton to="/">Search</NavLinkButton>
     <NavLinkButton to="/featured">Featured</NavLinkButton>
